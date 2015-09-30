fb8151dd6c3a3ab7f94226bff159a2cf2e5b163a
joed7 <rohitgupta0810@gmail.com>
2015-09-29 20:04:16 -0600

    fixing wiki url

--- scraping.py
+++ scraping.py
@@ -3,26 +3,27 @@
 from bs4 import BeautifulSoup
 
 import requests
-
-
+fobj1=open('teams','w')
+fobj2=open('team_links','w')
+wiki='https://en.wikipedia.org/'
 r = requests.get('https://en.wikipedia.org/wiki/National_Hockey_League') 
 
 
 data = r.text
 
 soup = BeautifulSoup(data)
-#Extract the table
+
 team_table = soup.find('table','navbox wikitable')
 
-#Extract the rows
 all_rows=team_table.find_all('tr')
 
 for row in all_rows:
-	#Extract all the columns for the row
     col=row.find_all('td')
     if len(col) > 0:
         print col[0].text,
         print col[0].find('a').get('href')
+        fobj1.write(col[0].text+"\n")
+        fobj2.write(wiki+col[0].find('a').get('href')+"\n")
     else:
         pass
         #print row    
